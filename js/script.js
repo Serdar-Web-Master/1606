f0901d6108764e356eb1d7377c98915e15555e5b
Serdar <serdarcholukov@yandex.com>
2022-06-16 23:53:41 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+import workingLink from "./ssylki.js";
+import { admin , aiya , client1 , client2 , client3 , client4 , client5 , client6 , client7 , client8 , client9 , client10 } from "./clients.js";
+
+const wrapper = document.querySelector ('.wrapper');
+const block1 = document.querySelector(".block1");
+const block2 = document.querySelector(".block2");
+const key = document.querySelector(".key");
+const pay__kod = document.querySelector(".pay__kod");
+const btn__ok = document.querySelector(".btn__ok");
+const setting__block = document.querySelector ('.setting__block');
+const seting__block = document.querySelector ('.seting__block');
+
+setTimeout(f1, 5000);
+
+function f1() {
+  block1.classList.add("move");
+}
+
+key.addEventListener("click", f2);
+
+function f2() {
+  block2.classList.add("move");
+}
+btn__ok.addEventListener("click", f3);
+
+function f3() {
+  const kod = pay__kod.value;
+  if (
+    kod == admin ||
+    kod == aiya ||
+    kod == client1 ||
+    kod == client2 ||
+    kod == client3 ||
+    kod == client4 ||
+    kod == client5 ||
+    kod == client6 ||
+    kod == client7 ||
+    kod == client8 ||
+    kod == client9 ||
+    kod == client10
+  ) {
+    workingLink.click();
+  } else {
+    alert("Код Неверный");
+  }
+}
+
+setting__block.addEventListener("click",f5);
+
+function f5 (){
+  wrapper.classList.add("display__none");
+  seting__block.classList.add("display__block");
+}
---
+++ js/ssylki.js
@@ -0,0 +1,4 @@
+const workingLink = document.createElement("a");
+workingLink.setAttribute("href", "https://drive.google.com/drive/folders/1We4WH3OePMOlyY2Onmt1p7a80iw1iJPz?usp=sharing");
+
+export default workingLink;
